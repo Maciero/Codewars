468c9ae50dc094a5a5705f08c1072d40603cdfb3
Maciero <124499484+Maciero@users.noreply.github.com>
2023-05-05 12:26:30 +0200

    RemoveFirstandLastCharacter added

---
+++ kyy8/RemoveFirstandLastCharacter.java
@@ -0,0 +1,8 @@
+package org.Codewars.kyy8;
+
+public class RemoveFirstandLastCharacter {
+    public static String remove(String str) {
+
+        return str.substring(1, str.length() - 1);
+    }
+}
