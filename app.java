c8a5839c8747abcc0968db2a3f5519b8659356b1
Maciero <124499484+Maciero@users.noreply.github.com>
2023-05-17 20:13:52 +0200

    app updated

--- app.java
+++ app.java
@@ -1,23 +1,25 @@
 package org.Codewars;
 
-import org.Codewars.kyu6.Persist;
-import org.Codewars.kyu7.ListFiltering;
-import org.Codewars.kyy8.RentalCarCost;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class app {
     public static void main(String[] args) {
 
+
+
+
         List<Object> list = new ArrayList<>();
-        list.add("a");
+//        list.add("a");
         list.add(3);
         list.add(4);
         list.add(5);
-        int a[] = {3,4,3,1,4};
-//        System.out.println(Arrays.toString(BeginnerLostWithoutAMap.map(a)));
+        list.add(5);
+        list.add(5);
+        int a[] = {3, 4, 3, 1, 4};
+        int b[] = {1, -2, 1, 1, 2};
 
-        System.out.println(Persist.persistence(4 ));
+        System.out.println();
+
     }
 }
