2e6803e135f70cd8128b36a8a88b88908ab65246
Maciero <124499484+Maciero@users.noreply.github.com>
2023-05-29 13:37:35 +0200

    SumArray added

---
+++ kyy8/SumArray.java
@@ -0,0 +1,9 @@
+package org.Codewars.kyy8;
+
+import java.util.Arrays;
+
+public class SumArray {
+    public static double sum(double[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+}
